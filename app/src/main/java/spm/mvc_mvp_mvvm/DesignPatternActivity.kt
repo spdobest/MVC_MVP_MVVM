9a3b08f8486c2b450ffa1bbb72b509e4fec28423
SIBAPRASAD <neosoft.sibaprasad@angelbroking.com>
2018-04-02 12:41:02 +0530

    First COmmiuts

---
+++ app/src/main/java/spm/mvc_mvp_mvvm/DesignPatternActivity.kt
@@ -0,0 +1,12 @@
+package spm.mvc_mvp_mvvm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DesignPatternActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_design_pattern)
+    }
+}
