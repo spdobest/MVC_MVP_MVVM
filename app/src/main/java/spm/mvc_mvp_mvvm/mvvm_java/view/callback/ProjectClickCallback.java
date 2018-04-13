3ff847a7032d274fa78057ffcd1b10631031549d
SIBAPRASAD <neosoft.sibaprasad@angelbroking.com>
2018-04-13 18:27:26 +0530

    mvvm added

---
+++ app/src/main/java/spm/mvc_mvp_mvvm/mvvm_java/view/callback/ProjectClickCallback.java
@@ -0,0 +1,7 @@
+package spm.mvc_mvp_mvvm.mvvm_java.view.callback;
+
+import com.example.test.mvvmsampleapp.service.model.Project;
+
+public interface ProjectClickCallback {
+    void onClick(Project project);
+}
