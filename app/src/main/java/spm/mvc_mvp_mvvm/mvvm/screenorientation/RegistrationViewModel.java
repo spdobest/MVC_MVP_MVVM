55e5f93e6b55bbf19687e3f939198fd65f1ae679
SIBAPRASAD <neosoft.sibaprasad@angelbroking.com>
2018-09-18 20:30:21 +0530

    Saving SCreen orientation State

--- app/src/main/java/spm/mvc_mvp_mvvm/mvvm/screenorientation/RegistrationViewModel.java
+++ app/src/main/java/spm/mvc_mvp_mvvm/mvvm/screenorientation/RegistrationViewModel.java
@@ -2,23 +2,26 @@ package spm.mvc_mvp_mvvm.mvvm.screenorientation;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LifecycleObserver;
 import android.arch.lifecycle.LiveData;
 import android.databinding.ObservableField;
 import android.support.annotation.NonNull;
 
-public class NewsViewModel extends AndroidViewModel {
+public class RegistrationViewModel extends AndroidViewModel implements LifecycleObserver {
     // https://android.jlelse.eu/android-architecture-pattern-components-mvvm-livedata-viewmodel-lifecycle-544e84e85177
-    private final LiveData<RegistrationModel> registerLiveData;
+    private LiveData<RegistrationModel> registerLiveData;
 
     public ObservableField<RegistrationModel> news = new ObservableField<>();
 
-    public NewsViewModel(@NonNull Application application) {
+    public RegistrationViewModel(@NonNull Application application) {
         super(application);
         // a differnt source can be passed, here i am passing techcrunch
-        registerLiveData = NewsRepository.getInstance().getNews("techcrunch");
     }
-
     public LiveData<RegistrationModel> getObservableRegistrationModel() {
         return registerLiveData;
+    }
+
+    public void setObservableRegistrationModel(LiveData<RegistrationModel> registrationModel) {
+        this.registerLiveData = registrationModel;
     }
 }
--- app/src/main/java/spm/mvc_mvp_mvvm/mvvm/screenorientation/ScreenOrientationActivity.kt
+++ app/src/main/java/spm/mvc_mvp_mvvm/mvvm/screenorientation/ScreenOrientationActivity.kt
@@ -1,14 +1,59 @@
 package spm.mvc_mvp_mvvm.mvvm.screenorientation
 
+import android.arch.lifecycle.LifecycleOwner
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-
+import kotlinx.android.synthetic.main.activity_screen_orientation.*
 import spm.mvc_mvp_mvvm.R
+import spm.mvc_mvp_mvvm.mvvm.mvvm.dto.CountryResult
+import spm.mvc_mvp_mvvm.mvvm.mvvm.viewModel.CountryViewModel
 
-class ScreenOrientationActivity : AppCompatActivity() {
+class ScreenOrientationActivity : AppCompatActivity(), LifecycleOwner {
+
+    lateinit var  registrationModel:RegistrationViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_screen_orientation)
+
+
+        registrationModel = ViewModelProviders.of(this).get(RegistrationViewModel::class.java)
+
+        if(registrationModel!=null){
+
+            val registerModelFromViewModel = registrationModel.getObservableRegistrationModel()
+
+            if(registerModelFromViewModel!=null && registerModelFromViewModel.value !=null) {
+
+                val reg = registerModelFromViewModel.value
+
+
+                editTextName.setText(reg?.name)
+                editTextEmailId.setText(reg?.emailId)
+                editTextMobileNumber.setText(reg?.mobileNumber)
+                editTextPassword.setText(reg?.password)
+            }
+        }
+
+
+        lifecycle.addObserver(registrationModel)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        var registerModel:RegistrationModel = RegistrationModel()
+        registerModel.name = editTextEmailId.text.toString()
+        registerModel.emailId = editTextEmailId.text.toString()
+        registerModel.mobileNumber = editTextEmailId.text.toString()
+        registerModel.password = editTextEmailId.text.toString()
+
+        val data = MutableLiveData<RegistrationModel>()
+
+        data.value = registerModel
+        registrationModel.setObservableRegistrationModel(data)
     }
 }
